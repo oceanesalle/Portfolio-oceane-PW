775ab98ad132bf22724139706b8f7bd9b02d35e4
oceansalle <oceanesalleiae@gmail.com>
2022-03-23 19:29:58 +0100

    portfolio

--- js/main.js
+++ js/main.js
@@ -75,7 +75,18 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+let swiper = new Swiper('.portfolio-container', {
+    cssMode: true,
+    loop: true, 
+    mavigation: {
+    nextE1: '.swiper-button-next', 
+    prevE1: '.swiper-button-prev', 
+    }, 
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true, 
+    }, 
+}); 
 
 /*==================== TESTIMONIAL ====================*/
 
