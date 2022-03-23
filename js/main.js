52af38115734ea86c1b4ac047745f757f8ebe70f
oceansalle <oceanesalleiae@gmail.com>
2022-03-23 15:42:38 +0100

    services

--- js/main.js
+++ js/main.js
@@ -52,7 +52,27 @@ skillsHeader.forEach((el) => {
 
 
 /*==================== SERVICES MODAL ====================*/
+const modalView = document.querySelectorAll('.services__modal'), 
+      modalBtns = document.querySelectorAll('.services__button'), 
+      modalCloses = document.querySelectorAll('.services__modal-close')
 
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener('click', () => {
+    modal(i)
+  })
+})
+
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener('click', () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove('active-modal')
+    })
+  })
+})
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
